a5edea7f4df11db92522edcfd635263d35274ceb
Nico Martinucci <domenico.martinucci@gmail.com>
2022-11-16 11:33:55 -0800

    make get show function dynamic

--- tvmaze.js
+++ tvmaze.js
@@ -3,6 +3,10 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const $searchFormTerm = $("#searchForm-term");
+
+const SHOWS_BASE_URL = "http://api.tvmaze.com/search/shows";
+const EPISODES_BASE_URL = "http://api.tvmaze.com/shows/"
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -12,27 +16,11 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  let shows = await axios.get(SHOWS_BASE_URL, { params: { q: term } });
+  console.log(shows);
 }
 
 
